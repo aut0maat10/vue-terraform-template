2d21c5d706d52731c8c27d0278586da8ce7d94c1
Robin Pahlman <robin.pahlman@gmail.com>
2023-10-31 11:00:38 -0400

    terraform init

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
